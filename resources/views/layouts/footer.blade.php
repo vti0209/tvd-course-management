264028078733913a0c6e518f3ea5a8788c0ef6c9
vti0209 <hotrai84@gmail.com>
2026-04-02 10:19:42 +0700

    feat: add footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,43 @@
+<footer class="bg-dark text-white mt-5 pt-4">
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Cột 1 -->
+            <div class="col-md-4">
+                <h5 class="fw-bold">TVD</h5>
+                <p>
+                    Nền tảng học tập trực tuyến giúp bạn làm chủ công nghệ,
+                    xây dựng sự nghiệp và thay đổi tương lai.
+                </p>
+            </div>
+
+            <!-- Cột 2 -->
+            <div class="col-md-4">
+                <h5 class="fw-bold">Về chúng tôi</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-white text-decoration-none">Giới thiệu chung</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Đội ngũ giảng viên</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Cơ hội việc làm</a></li>
+                </ul>
+            </div>
+
+            <!-- Cột 3 -->
+            <div class="col-md-4">
+                <h5 class="fw-bold">Kết nối</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-white text-decoration-none">Facebook Group</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Kênh Youtube</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Cộng đồng Discord</a></li>
+                </ul>
+            </div>
+
+        </div>
+
+        <!-- Copyright -->
+        <div class="text-center mt-4 pb-3 border-top pt-3">
+            © 2026 TVD. All rights reserved.
+        </div>
+
+    </div>
+</footer>
