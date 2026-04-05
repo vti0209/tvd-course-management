3f2567f4eee4c6fe049a5efebfe336e3fc04eae4
hothivai <hothivai22@gmail.com>
2026-04-05 21:43:05 +0700

    auth done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -26,7 +26,7 @@ class AuthController extends Controller
         $request->validate([
             'name' => 'required',
             'email' => 'required|email|unique:users',
-            'password' => 'required|min:6'
+            'password' => 'required|min:6|confirmed'
         ]);
 
         User::create([
