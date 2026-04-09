0a84aafb576ab1051734615bc569bd9fdff0bf71
vti0209 <hotrai84@gmail.com>
2026-04-09 10:40:04 +0700

    feat: done

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Str;
 
@@ -43,5 +44,9 @@ class Course extends Model
     public function users()
     {
         return $this->belongsToMany(User::class, 'course_user');
+    }
+    public function lessons(): HasMany
+    {
+        return $this->hasMany(Lesson::class);
     }
 }
